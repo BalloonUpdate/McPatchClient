4f5a095ed69a9c242f56c2dd08921c7cc6e74087
敖律风 <x123456789fy@outlook.com>
2023-05-20 10:42:30 +0800

    整理代码，生成测试类

---
+++ src/main/kotlin/mcpatch/WorkThreadTest.kt
@@ -0,0 +1,214 @@
+package mcpatch
+
+import org.junit.jupiter.api.Test
+
+class WorkThreadTest {
+
+    @Test
+    fun blockedOn() {
+    }
+
+    @Test
+    fun currentThread() {
+    }
+
+    @Test
+    fun yield() {
+    }
+
+    @Test
+    fun sleep() {
+    }
+
+    @Test
+    fun testSleep() {
+    }
+
+    @Test
+    fun testClone() {
+    }
+
+    @Test
+    fun start() {
+    }
+
+    @Test
+    fun run() {
+    }
+
+    @Test
+    fun stop() {
+    }
+
+    @Test
+    fun testStop() {
+    }
+
+    @Test
+    fun interrupt() {
+    }
+
+    @Test
+    fun interrupted() {
+    }
+
+    @Test
+    fun isInterrupted() {
+    }
+
+    @Test
+    fun destroy() {
+    }
+
+    @Test
+    fun isAlive() {
+    }
+
+    @Test
+    fun suspend() {
+    }
+
+    @Test
+    fun resume() {
+    }
+
+    @Test
+    fun setPriority() {
+    }
+
+    @Test
+    fun getPriority() {
+    }
+
+    @Test
+    fun setName() {
+    }
+
+    @Test
+    fun getName() {
+    }
+
+    @Test
+    fun getThreadGroup() {
+    }
+
+    @Test
+    fun activeCount() {
+    }
+
+    @Test
+    fun enumerate() {
+    }
+
+    @Test
+    fun countStackFrames() {
+    }
+
+    @Test
+    fun join() {
+    }
+
+    @Test
+    fun testJoin() {
+    }
+
+    @Test
+    fun testJoin1() {
+    }
+
+    @Test
+    fun dumpStack() {
+    }
+
+    @Test
+    fun setDaemon() {
+    }
+
+    @Test
+    fun isDaemon() {
+    }
+
+    @Test
+    fun checkAccess() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+
+    @Test
+    fun getContextClassLoader() {
+    }
+
+    @Test
+    fun setContextClassLoader() {
+    }
+
+    @Test
+    fun holdsLock() {
+    }
+
+    @Test
+    fun getStackTrace() {
+    }
+
+    @Test
+    fun getAllStackTraces() {
+    }
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    fun getState() {
+    }
+
+    @Test
+    fun setDefaultUncaughtExceptionHandler() {
+    }
+
+    @Test
+    fun getDefaultUncaughtExceptionHandler() {
+    }
+
+    @Test
+    fun getUncaughtExceptionHandler() {
+    }
+
+    @Test
+    fun setUncaughtExceptionHandler() {
+    }
+
+    @Test
+    fun processQueue() {
+    }
+
+    @Test
+    fun getDownloadedVersionCount() {
+    }
+
+    @Test
+    fun setDownloadedVersionCount() {
+    }
+
+    @Test
+    fun testRun() {
+    }
+
+    @Test
+    fun getWindow() {
+    }
+
+    @Test
+    fun getOptions() {
+    }
+
+    @Test
+    fun getUpdateDir() {
+    }
+
+    @Test
+    fun getProgDir() {
+    }
+}
