33f8348d07fe83ef66d2cae72bf1f5d2c4b13fd2
敖律风 <x123456789fy@outlook.com>
2023-05-20 15:08:36 +0800

    Publish Build Scans from GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
-
+//根项目名称
 rootProject.name = "McPatchClient"
+//插件列表
+plugins {
+    id("com.gradle.enterprise") version("3.13.2")
+}
 
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
